c02b9fd0ca8b8f56c0c9a9532d9880d0ceaf7d00
Austin Aitken <austin.aitken@hotmail.com>
2021-06-26 19:54:32 -0230

    Began GroceryQueue implementation

--- GroceryQueue.java
+++ GroceryQueue.java
@@ -1,7 +1,25 @@
+
+/*
+    Like bank queue but semaphore is just 1
+*/
 public class GroceryQueue {
 
-    public GroceryQueue() {
-        
+    public GroceryQueue(int maxQueueSize) {
+        this.maxQueueSize = maxQueueSize;
     }
 
+    public void joinQueue(Customer customer) {
+
+    }
+
+    public int getCurrentQueueSize() {
+        return currentQueueSize;
+    }
+
+    public int getMaxQueueSize() {
+        return maxQueueSize;
+    }
+
+    private int currentQueueSize;
+    private int maxQueueSize;
 }
--- GroceryQueues.java
+++ GroceryQueues.java
@@ -10,13 +10,28 @@ public class GroceryQueues {
     public GroceryQueues(int numOfQueues, int maxQueueSize) {
         this.maxQueueSize = maxQueueSize;
         for (int i = 0; i < numOfQueues; i++) {
-            GroceryQueue queue = new GroceryQueue();
+            GroceryQueue queue = new GroceryQueue(maxQueueSize);
             queues.add(queue);
         }
     }
 
     public void joinQueue(Customer customer) {
-
+        int minQueueSize = queues.get(0).getCurrentQueueSize();
+        int minQueueIndex = 0;
+        for (int i = 1; i < queues.size(); i++) {
+            if (queues.get(i).getCurrentQueueSize() < minQueueSize) {
+                minQueueSize = queues.get(i).getCurrentQueueSize();
+                minQueueIndex = i;
+            }
+        }
+        if (maxQueueSize < minQueueSize + 1) {
+            /*
+                TODO: Customer waits 10s before leaving
+            */
+        } else {
+            queues.get(minQueueIndex).joinQueue(customer);
+        }
+        allCustomers.add(customer);
     }
 
     public int getCustomersLeftTotal() {
@@ -28,5 +43,6 @@ public class GroceryQueues {
     }
 
     private ArrayList<GroceryQueue> queues = new ArrayList<GroceryQueue>();
+    private ArrayList<Customer> allCustomers = new ArrayList<Customer>();
     private int maxQueueSize;
 }
